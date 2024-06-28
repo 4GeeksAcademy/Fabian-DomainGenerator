0b9a1b9309deba98c12c18be07cd40e9182d1e2b
fabruizb <166636528+fabruizb@users.noreply.github.com>
2024-06-28 19:24:34 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domain").innerHTML = generateDomain();
 };
+
+const generateDomain = () => {
+  let pronoun = ["the", "our", "we", "your"];
+  let adj = ["great", "big", "small", "ugly"];
+  let noun = ["jogger", "raco", "do", "mov"];
+  let ext = [".com", ".es", ".it", ".on", ".org"];
+
+  const pronounIndex = Math.floor(Math.random() * pronoun.length);
+  const adjIndex = Math.floor(Math.random() * adj.length);
+  const nounIndex = Math.floor(Math.random() * noun.length);
+  const extIndex = Math.floor(Math.random() * ext.length);
+
+  const domain = `${pronoun[pronounIndex]}${adj[adjIndex]}${noun[nounIndex]}${ext[extIndex]}`;
+  return domain;
+};
+
+const refreshButton = document.getElementById("refreshButton");
+
+refreshButton.addEventListener("click", function() {
+  location.reload();
+});
